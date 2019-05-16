c1df2c9b24b2c968aa3ffec43c61ddd56b329ca4
nikhila.ghai <nikhila.ghai@cgi.com>
2019-05-16 16:09:29 +0200

    Adding project skeleton

---
+++ src/main/java/org/cgi/rabobank/candidateconfigserver/CandidateConfigServerApplication.java
@@ -0,0 +1,15 @@
+package org.cgi.rabobank.candidateconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CandidateConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateConfigServerApplication.class, args);
+	}
+
+}
